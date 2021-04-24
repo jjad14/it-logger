eaf508212b5c6bace9a20e94a454e00c650ba86a
Dennis <dmarlo96@live.ca>
2021-04-24 18:29:28 -0400

    SearchBar and Logs Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+
 import './App.css';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
 const App = () => {
 
@@ -12,9 +15,12 @@ const App = () => {
   });
   
   return (
-    <div className="App">
-      My App
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
   );
 };
 
--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 const SearchBar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav style={{ marginBottom: '30px'}} className="blue-grey">
+            <div className="nav-wrapper">
+                <form>
+                    <div className="input-field">
+                    <input id="search" type="search" />
+                    <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
+                    <i className="material-icons">close</i>
+                    </div>
+                </form>
+            </div>
+        </nav>
     );
 };
 
